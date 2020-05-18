f1a4e1412ee2c8ba8a15635a5e921623e5a9ce24
timo_falcke <timo.falcke@gmail.com>
2020-05-18 22:40:09 +0200

    initial commit

---
+++ Imkertagebuch/Assets/InputFieldValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InputFieldValidator : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] Color normalColor;
+    [SerializeField] Color filledColor;
+
+    TMP_InputField inputField;
+    Image inputFieldBg;
+
+    private void Awake()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        inputFieldBg = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        inputField.onEndEdit.AddListener(OnEndEdit);
+        inputField.onValidateInput += ValidateInput;
+    }
+
+    private char ValidateInput(string text, int charIndex, char addedChar)
+    {
+        // custom validation here
+        return addedChar;
+    }
+
+    private void OnEndEdit(string value)
+    {
+        if (value != "")
+            inputFieldBg.color = filledColor;
+        else
+            inputFieldBg.color = normalColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        inputFieldBg.color = normalColor;
+    }
+}
---
+++ Imkertagebuch/Assets/Scripts/HiveData.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct HiveData
+{
+    [Header("General")]
+    [Tooltip("Beutennummer")]
+    public string number;
+    [Tooltip("Standmaß")]
+    public string size;
+    [Tooltip("Ort")]
+    public int location;
+
+    [Header("Queen")]
+
+    [Tooltip("Zuchtbuch-Nr.")]
+    public int queenBreedingBookNumber;
+    [Tooltip("Zeichen")]
+    public string queenSign;
+    [Tooltip("Jahrgang")]
+    public string queenBirthyear;
+    [Tooltip("Rasse-Linie")]
+    public string queenRace;
+
+    [Header("Drones")]
+
+    [Tooltip("Zuchtbuch-Nr.")]
+    public int dronesBreedingBookNumber;
+    [Tooltip("gemeinsame Mutter")]
+    public int dronesSharedMother;
+    [Tooltip("Großmutter")]
+    public int dronesGrandmother;
+    [Tooltip("Belegstelle")]
+    public int dronesPlaceOfReference;
+    [Tooltip("Rasse-Linie")]
+    public int dronesRace;
+    [Tooltip("Zugesetzt")]
+    public System.DateTime dronesAddedDate;
+
+
+}
